95f89e1ccb380215f268bbe11d2f7cf404075b95
Natalie Halawani <hala1224@gmail.com>
2018-10-08 23:57:39 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
 // Write your code in this file!
 function scuberGreetingForFeet(ride) {
-  if (ride <= 400)  {
+/*  if (ride <= 400)  {
     return 'This one is on me!';
   } 
   if (ride > 2000 || ride <= 2500) {
     return 'I will gladly take your thirty bucks.';
-  } 
+  } */
   if (ride >2500) {
     return 'No can do.';
   }
