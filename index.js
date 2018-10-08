e500bed3bcf9750e335caceada49c8c451933ed6
Natalie Halawani <hala1224@gmail.com>
2018-10-08 23:45:22 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
 // Write your code in this file!
+function scuberGreetingForFeet(ride) {
+  if (ride <= 400)  {
+    return 'This one is on me!';
+  } else if ({
+    
+  }
+    
+
+    it('charges 30 dollars for a distance over 2000 feet', function () {
+      expect(scuberGreetingForFeet(2001)).to.equal('I will gladly take your thirty bucks.');
+    });
+
+    it('does not allow rides over 2500 feet', function () {
+      expect(scuberGreetingForFeet(2501)).to.equal('No can do.');
+    });
+  });
